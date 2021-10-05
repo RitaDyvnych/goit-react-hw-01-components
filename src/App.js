1b2a3e3feaabdb2f98a7135d3f98fe28275f043b
RitaDyvnych <m.dyvnych@gmail.com>
2021-10-05 22:29:42 +0300

    деплой проекта

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import user from "./data/user.json";
-import User from "./components/User";
-import statisticalData from "./data/statistical-data.json";
-import Statistics from "./components/Statistics";
-import friends from "./data/friends.json";
-import FriendList from "./components/FriendList";
-import items from "./data/transactions.json";
-import Transaction from "./components/Transaction";
+import user from "./components/user/user.json";
+import User from "./components/user/User";
+import statisticalData from "./components/statistics/statistical-data.json";
+import Statistics from "./components/statistics/Statistics";
+import friends from "./components/friends/friends.json";
+import FriendList from "./components/friends/FriendList";
+import items from "./components/transaction/transactions.json";
+import Transaction from "./components/transaction/Transaction";
 
 function App() {
   return (
